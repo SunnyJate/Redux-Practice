423dcb351787830206df9b421c15fb6283a7c862
Sunny Jate <sunnyjate2007@gmail.com>
2024-08-26 12:06:25 +0530

    Count Function

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const[count, setCount]= useState(0)
+  function incr(){
+    setCount(count+1)
+  }
+  function decr(){
+    setCount(count-1)
+  }
   return (
     <div>
-      
+      <h1>{count}</h1>
+      <button onClick={incr}>+</button>
+      <br/>
+      <button onClick={decr}>-</button>
     </div>
   )
 }
